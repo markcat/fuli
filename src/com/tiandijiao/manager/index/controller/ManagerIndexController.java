0b5371c0554fe0860e3fc1302a1d34313aa939e0
markcat <mark3@126.com>
2016-05-28 18:58:51 +0800

    管理后台首页添加

--- src/com/tiandijiao/manager/index/controller/ManagerIndexController.java
+++ src/com/tiandijiao/manager/index/controller/ManagerIndexController.java
@@ -3,8 +3,15 @@ package com.tiandijiao.manager.index.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
-@RequestMapping("/manager/article")
-public class ManagerArticleController {
 
+@Controller
+@RequestMapping("/manager/index")
+
+
+public class ManagerIndexController {
+
+	@RequestMapping(value={"", "/"})
+	public String index(){
+		return "/pages/manager/index";
+	}
 }
